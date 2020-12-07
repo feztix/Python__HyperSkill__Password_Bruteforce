9d23a56b27803f337779e2e27228c5ae4099d1d7
feztix <pan0v.s.s@yandex.ru>
2020-12-07 09:55:38 +0300

    Stage 1/5: Your program will receive command line arguments in this order:
    IP address
    port
    message for sending
    The algorithm is the following:
    
    Create a new socket.
    Connect to a host and a port using the socket.
    Send a message from the third command line argument to the host using the socket.
    Receive the server’s response.
    Print the server’s response.
    Close the socket.

---
+++ Password Hacker (1)/task/hacking/hack.py
@@ -0,0 +1,62 @@
+import argparse
+import socket
+import os
+import sys
+
+
+class PasswordHacker:
+    def __init__(self):
+        # For unpcak using args.Ip/Port/Message
+        args = self.init_cli()
+        self.establish_connection(args)
+
+    def init_cli(self):
+        # Create the parser
+        my_parser = argparse.ArgumentParser(description='Establishing a connection')
+
+        # Add the arguments
+        # my_parser.add_argument('Path', metavar='path', type=str,
+        #                        help='the path to .py file')
+        my_parser.add_argument("Ip", metavar='ip', choices=["localhost", "127.0.0.1"],
+                               help="You need to choose only one IP address form the list.")
+        my_parser.add_argument("Port", metavar="port", type=int,
+                               help="You need to input port")
+        my_parser.add_argument("Message", metavar="message", type=str,
+                               help="You need to input message for sending")
+
+        # Execute the parse_args() method
+        args = my_parser.parse_args()
+
+        # input_path = args.Path
+
+        # if not os.path.isdir(input_path):
+        #     print('The path specified does not exist')
+        #     sys.exit()
+        #
+        # print('\n'.join(os.listdir(input_path)))
+        return args
+
+    def establish_connection(self, args):
+        with socket.socket() as client_socket:
+            hostname = args.Ip
+            port = args.Port
+            address = (hostname, port)
+
+            client_socket.connect(address)
+
+            data = args.Message
+            data = data.encode()
+
+            client_socket.send(data)
+
+            response = client_socket.recv(1024)
+
+            response = response.decode()
+            print(response)
+
+    def init(self):
+        pass
+
+
+if __name__ == "__main__":
+    PasswordHacker().init()
